f146e19cc296b388ed7b1a5ada1fa003ee8b4e96
Rafnix Guzman <rafnixg@gmail.com>
2020-03-28 20:23:18 -0500

    [ADD] Agrega la clase borracho y sus metodos

---
+++ borracho.py
@@ -0,0 +1,26 @@
+"""Modulo borrachos."""
+import random
+
+
+class Borracho:
+    """Clase para los borrachos."""
+
+    def __init__(self, nombre):
+        """Inicializa el borracho."""
+        self.nombre = nombre
+
+
+class BorrachoTradicional(Borracho):
+    """Clase para el borracho tradicional."""
+
+    def __init__(self, name):
+        """Inicializa el borracho tradicional."""
+        super().__init__(name)
+
+    def camina():
+        """Retorna una tupla.
+
+        Calcula el valor aleatorio con la direccion
+        a donde se mueve el borracho.
+        """
+        return random.choice([(0, 1), (0, -1), (1, 0), (-1, 0)])
