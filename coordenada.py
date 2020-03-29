05de2814ca39c22c749438c3e93d41be7a83cd2c
Rafnix Guzman <rafnixg@gmail.com>
2020-03-28 20:23:34 -0500

    [ADD] Agrega la clase Coordendas y sus metodos

---
+++ coordenada.py
@@ -0,0 +1,20 @@
+"""Modulo de coordenadas."""
+
+
+class Coordenada:
+    """Clase para las coordenadas."""
+
+    def __init__(self, x, y):
+        """Inicializa las coordendas."""
+        self.x = x
+        self.y = y
+
+    def mover(self, delta_x, delta_y):
+        """Retorna una nueva Coordenada con la nueva posicion."""
+        return Coordenada(self.x + delta_x, self.y + delta_y)
+
+    def distancia(self, otra_coordenada):
+        """Calcula la distancia entre las coordenada."""
+        delta_x = self.x - otra_coordenada.x
+        delta_y = self.y - otra_coordenada.y
+        return (delta_x**2 + delta_y**2)**0.5
